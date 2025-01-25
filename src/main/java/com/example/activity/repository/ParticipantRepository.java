3a4b6aa888c2f07242d5a367a3c79da4fe043881
Ibrahim Osman <ibrahim_97_o@hotmail.de>
2025-01-26 00:27:31 +0100

    add findByName method

--- src/main/java/com/example/activity/repository/ParticipantRepository.java
+++ src/main/java/com/example/activity/repository/ParticipantRepository.java
@@ -3,5 +3,9 @@ package com.example.activity.repository;
 import com.example.activity.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface ParticipantRepository extends JpaRepository<Participant,Long> {
+    Optional<Participant> findByName(String name);
+
 }
