14f1a53824b01361627f3f2a344588f6e8bc686b
Ibrahim Osman <ibrahim_97_o@hotmail.de>
2025-01-30 02:40:49 +0100

    add findByNameAndActivity method

--- src/main/java/com/example/activity/repository/ParticipantRepository.java
+++ src/main/java/com/example/activity/repository/ParticipantRepository.java
@@ -2,10 +2,14 @@ package com.example.activity.repository;
 
 import com.example.activity.entity.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface ParticipantRepository extends JpaRepository<Participant,Long> {
     Optional<Participant> findByName(String name);
 
+    @Query("SELECT p FROM Participant p JOIN p.activities a WHERE p.name = :name AND a.id = :activityId")
+    Optional<Participant> findByNameAndActivity(@Param("name") String name, @Param("activityId") Long activityId);
 }
